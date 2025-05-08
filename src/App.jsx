60cdf950153ab63b57c046e28953f5e696963613
Calvin526 <calvinmgarcia@hotmail.com>
2025-05-08 17:01:10 -0400

    Removed TailwindCSS Logo & Resized name and view resume buttons

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,8 @@ function App() {
      <nav class="flex items-center justify-between flex-wrap bg-blue-500 p-5">
       <br />
   <div class="flex items-center flex-shrink-0 text-white mr-6 font-cabin">
-    <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
-    <a class="font-semibold text-3xl tracking-tight">
+    
+    <a class="font-semibold text-5xl tracking-tight">
         Calvin Garcia
       </a>
   </div>
@@ -34,7 +34,7 @@ function App() {
     
     </div>
     <div>
-      <a href="/Calvin_Garcia_Resume.pdf" class="inline-block text-lg px-6 py-3 leading-none border rounded text-white border-white hover:border-transparent hover:text-purple-500 hover:bg-white mt-4 lg:mt-0"  target="_blank" rel="noopener noreferrer"> View Resume</a>
+      <a href="/Calvin_Garcia_Resume.pdf" class="inline-block text-3xl px-6 py-3 leading-none border rounded text-white border-white hover:border-transparent hover:text-purple-500 hover:bg-white mt-4 lg:mt-0"  target="_blank" rel="noopener noreferrer"> View Resume</a>
     </div>
   </div>
 </nav>
